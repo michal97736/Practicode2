a4fa2731e05cb4107ef4af784e45d35af4d1a4a7
michal97736 <michal97736@gmail.com>
2023-05-15 21:19:10 +0300

    my commit message12

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-console.log("hi")
 
+
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 //rnd_sS41XQwghPonKfXcJFCwFlV8EgiCconst
-sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 sdk.auth('rnd_sS41XQwghPonKfXcJFCwFlV8EgiC');
-sdk.getServices({ limit: '20' })
+sdk.getServices({limit: '20'})
   .then(({ data }) => console.log(data))
   .catch(err => console.error(err));
